d8894dd9529ba3140acdc99f8a7803b946db3a7b
SergeyDavydkin <94360756+SergeyDavydkin@users.noreply.github.com>
2022-03-28 09:32:54 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,1 +1,19 @@
-// место для вашего кода
+#pragma once
+#include "transport_catalogue.h"
+#include "geo.h"
+#include <string>
+#include <vector>
+#include <iostream>
+
+namespace transport_catalogue::output {
+
+    void ProcessStatRequests(std::vector<std::string>& buffer,
+        TransportCatalogue& catalogue,
+        std::ostream& out = std::cout);
+
+    void PrintRouteInfo(RouteInfo& route, std::ostream& out);
+
+    void PrintStopInfo(std::string_view name, TransportCatalogue& catalogue, std::ostream& out);
+
+    void PrintEmpty(std::string_view type, std::string_view name, std::ostream& out);
+}
