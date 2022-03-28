9856935f13df6512bf5c5c7ddf834d9017e1bc45
SergeyDavydkin <94360756+SergeyDavydkin@users.noreply.github.com>
2022-03-28 09:31:57 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,1 +1,17 @@
-// место для вашего кода
+#include "transport_catalogue.h"
+#include "input_reader.h"
+#include "stat_reader.h"
+#include <sstream>
+
+using namespace transport_catalogue;
+
+int main() {
+    TransportCatalogue transport_catalogue;
+    std::vector<std::string> requests = input::FillInputBuffer(std::cin);
+    input::ProcessRequest(requests, transport_catalogue);
+
+    requests.clear();
+    requests = input::FillInputBuffer(std::cin);
+    output::ProcessStatRequests(requests, transport_catalogue);
+    return 0;
+}
