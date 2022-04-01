5bd027527e2116ca72bc9f5963d1ea03f02a7468
SergeyDavydkin <94360756+SergeyDavydkin@users.noreply.github.com>
2022-04-01 22:00:10 +0300

    Update input_reader.h

--- transport-catalogue/input_reader.h
+++ transport-catalogue/input_reader.h
@@ -11,21 +11,29 @@
 
 namespace transport_catalogue::input {
 
+	// считываем строку
 	std::string ReadLine();
 
+	// считываем первое число
 	int ReadLineWithNumber();
 
+	// разбиваю строку на слова
 	std::vector<std::string_view> SplitIntoWords(std::string_view text, std::string_view separator);
 
+	//формируем коордианаты, имя остановки и расстояние
 	std::tuple<std::string_view,
 		transport_catalogue::Coordinates,
 		std::vector<std::pair<std::string, size_t>>> ProcessStopRequest(std::string_view stop);
 
+	// формирую маршруты
 	std::tuple<std::string_view, std::vector<std::string_view>, bool> ProcessRouteRequest(std::string_view route);
 
+	//формируем из строки на пару: остановка - расстояние
 	std::pair<std::string, size_t> ParseDistanceBetweenStops(std::string_view);
 
+	// формирует буфер запросов
 	std::vector<std::string> FillInputBuffer(std::istream&);
 
+	// выполняем запросы из буфера
 	void ProcessRequest(std::vector<std::string>& buffer, transport_catalogue::TransportCatalogue& catalogue);
 }
