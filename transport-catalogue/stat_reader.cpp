300e5f3ae32b2b419d07dc2085d9bac9b13bad0c
SergeyDavydkin <94360756+SergeyDavydkin@users.noreply.github.com>
2022-04-01 23:00:31 +0300

    Update stat_reader.cpp

--- transport-catalogue/stat_reader.cpp
+++ transport-catalogue/stat_reader.cpp
@@ -4,21 +4,22 @@ using namespace std::literals;
 
 namespace transport_catalogue::output {
 
+    
     void ProcessStatRequests(std::vector<std::string>& buffer,
         transport_catalogue::TransportCatalogue& catalogue,
         std::ostream& out) {
-        for (std::string_view str : buffer) {
-            if (str.substr(0, "Bus"s.size()) == "Bus"s) {
-                str.remove_prefix("Bus "s.size());
-                if (catalogue.FindRoute(str) == nullptr) {
-                    PrintEmpty("Bus"s, str, out);
+        for (std::string_view str : buffer) {               //проходим по буферу запроса
+            if (str.substr(0, "Bus"s.size()) == "Bus"s) {   // если "Bus"
+                str.remove_prefix("Bus "s.size());          // оставляем название автобуса/маршрута
+                if (catalogue.FindRoute(str) == nullptr) {  //если каталог не содержит маршрута
+                    PrintEmpty("Bus"s, str, out);           // петчатаем его отсутствие
                 }
-                else {
-                    auto route_info = catalogue.GetRouteInfo(str);
-                    PrintRouteInfo(route_info, out);
+                else {                                              // если содержит маршрут 
+                    auto route_info = catalogue.GetRouteInfo(str); //присваиваем информацию о маршруте
+                    PrintRouteInfo(route_info, out);               // выводим  информацию о маршруте
                 }
             }
-            else if (str.substr(0, "Stop"s.size()) == "Stop"s) {
+            else if (str.substr(0, "Stop"s.size()) == "Stop"s) {  // аналогично для остановок
                 str.remove_prefix("Stop "s.size());
                 if (catalogue.FindStop(str) == nullptr) {
                     PrintEmpty("Stop"s, str, out);
@@ -30,14 +31,15 @@ namespace transport_catalogue::output {
         }
     }
 
-    void PrintRouteInfo(RouteInfo& route, std::ostream& out) {
-        out << "Bus "s << route.name << ": "s
-            << route.stops << " stops on route, "s
-            << route.unique_stops << " unique stops, "s
-            << route.route_length << " route length, "s
-            << route.curvature << " curvature"s << std::endl;
+    void PrintRouteInfo(RouteInfo& route, std::ostream& out) { // формирование выходных данных о маршруте
+        out << "Bus "s << route.name << ": "s                  // вывод название автобуса/маршрута
+            << route.stops << " stops on route, "s             // вывод количества остановок
+            << route.unique_stops << " unique stops, "s         // вывод количества уникальных остановок
+            << route.route_length << " route length, "s         // длина маршрута
+            << route.curvature << " curvature"s << std::endl;   //кривизна маршрута
     }
 
+    //вывод остановок для запроса по автобусам
     void PrintStopInfo(std::string_view name, transport_catalogue::TransportCatalogue& catalogue, std::ostream& out) {
         const std::set<std::string>& stops = catalogue.GetStopInfo(name);
         out << "Stop "s << name << ":"s;
@@ -53,6 +55,7 @@ namespace transport_catalogue::output {
         }
     }
 
+    //вывод отсутствия маршрутов, автобусов, остановок 
     void PrintEmpty(std::string_view type, std::string_view name, std::ostream& my_output) {
         my_output << type << ' ' << name << ": "s << "not found"s << std::endl;
     }
