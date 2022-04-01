c71a9f9854dff795787a5e40f417c5e9723e904a
SergeyDavydkin <94360756+SergeyDavydkin@users.noreply.github.com>
2022-04-01 22:48:07 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -7,13 +7,17 @@
 
 namespace transport_catalogue::output {
 
+    //формирование буфера данных вывода
     void ProcessStatRequests(std::vector<std::string>& buffer,
         TransportCatalogue& catalogue,
         std::ostream& out = std::cout);
 
+    // вывод информации о маршруте
     void PrintRouteInfo(RouteInfo& route, std::ostream& out);
 
+    // вывод информации об остановках
     void PrintStopInfo(std::string_view name, TransportCatalogue& catalogue, std::ostream& out);
 
+    //вывод отсутствия маршрута/остановок
     void PrintEmpty(std::string_view type, std::string_view name, std::ostream& out);
 }
