587a513d344d22993350802cddde67dd410f820f
SergeyDavydkin <94360756+SergeyDavydkin@users.noreply.github.com>
2022-04-09 20:29:23 +0300

    Create json.h

---
+++ transport-catalogue/json.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+#include <variant>
+
+namespace json {
+
+    class Node;
+    using Dict = std::map<std::string, Node>;
+    using Array = std::vector<Node>;
+    using NodeType = std::variant<std::nullptr_t, Array, Dict, bool, int, double, std::string>;
+
+    // Эта ошибка должна выбрасываться при ошибках парсинга JSON
+    class ParsingError : public std::runtime_error {
+    public:
+        using runtime_error::runtime_error;
+    };
+
+    class Node final : private NodeType {
+    public:
+        using variant::variant;
+        using Value = variant;
+        
+        Node(Value& value);
+
+        bool IsNull() const;
+        bool IsInt() const;
+        bool IsDouble() const;
+        bool IsPureDouble() const;
+        bool IsBool() const;
+        bool IsString() const;
+        bool IsArray() const;
+        bool IsMap() const;
+
+        bool AsBool() const;
+        int AsInt() const;
+        double AsDouble() const;
+        const std::string& AsString() const;
+        const Array& AsArray() const;
+        const Dict& AsMap() const;
+
+        const Value& GetValue() const;
+
+        bool operator==(const Node& rhs) const;
+        bool operator!=(const Node& rhs) const;
+    };
+
+    class Document {
+    public:
+        explicit Document(Node root);
+
+        const Node& GetRoot() const;
+
+        bool operator==(const Document& rhs);
+        bool operator!=(const Document& rhs);
+
+    private:
+        Node root_;
+
+    };
+
+    Document Load(std::istream& input);
+
+    void Print(const Document& doc, std::ostream& output);
+    void PrintNode(const Node& node, std::ostream& output);
+}  // namespace json
