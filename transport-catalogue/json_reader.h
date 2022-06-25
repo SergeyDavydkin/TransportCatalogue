afe42979af70d4c58e2f736c5aaa042ce755de3d
SergeyDavydkin <94360756+SergeyDavydkin@users.noreply.github.com>
2022-06-25 08:43:58 +0300

    Update json_reader.h

--- transport-catalogue/json_reader.h
+++ transport-catalogue/json_reader.h
@@ -1,34 +1,101 @@
 #pragma once
 
-#include "transport_catalogue.h"
-#include "request_handler.h"
 #include "json.h"
-#include "domain.h"
+#include "json_builder.h"
+#include "request_handler.h"
 #include "map_renderer.h"
+//#include "svg.h"
+#include "transport_catalogue.h"
+#include "transport_router.h"
+#include "serialization.h"
+
+#include <sstream>
+#include <variant>
+#include <iomanip>
+#include <iostream>
+
 
 namespace json_reader {
 
-	using DataBasePtr = transport_catalogue::TransportCatalogue*;
-	using MapRenderPtr = renderer::MapRenderer*;
-	class JsonReader {
-	public:
-		explicit JsonReader(std::istream& input, DataBasePtr catalogue, MapRenderPtr map_renderer);
+    using namespace json;
 
-		void GetOutput(std::ostream& output);
-	private:
-		json::Document input_;
-		DataBasePtr catalogue_ptr_;
-		MapRenderPtr map_renderer_ptr_;
+    namespace detail {
+        
+        enum class RequestType {
+            BASE = 0, 
+            STAT,
+            RENDER,
+            EMPTY
+        };
+        enum class request_type { BUS, STOP, MAP, ROUTE, EMPTY };
+        
+        struct Request {
+            RequestType e_type = RequestType::EMPTY;
+            request_type type = request_type::EMPTY;
+            virtual ~Request() = default;
+        };
+        
+        struct RequestStatRoute {
+            std::string from;
+            std::string to;
+        };
+        
+        struct RequestStat : public Request {
+            std::string name; 
+            int id = 0;
+            RequestStatRoute route;
+        };
+        
+        struct RequestStop : public Request {
+            std::string name;
+            double latitude = 0;
+            double longitude = 0;
+            std::map<std::string, int> road_distances;
+        };
+        
+        struct RequestBus : public Request {
+            std::string name;
+            bool is_roundtrip = false;
+            std::vector<std::string> stops;
+        };
+        
+        std::map<std::string, int> DistBtwStops(const Dict& dic); // Обработать расстояния до остановок для BaseStop()
+        RequestStop BaseStop(const Dict& dic);
+        std::vector<std::string> StopsForBus(const Array& arr); // Обработать расстояния до остановок для BaseBus()
+        RequestBus BaseBus(const Dict& dic);
+        
+        RequestStat Stat(const Dict& dic);
 
-		// возвращает ответ на запрос инфромации о маршруте
-		json::Dict GetRoutes_(Handler::RequestHandler& handler, const json::Node& request);
-		// возвращает ответ на запрос инфромации об остановке
-		json::Dict GetStops_(Handler::RequestHandler& handler, const json::Node& request);
-		// возвращает ответ на запрос построения карты маршрутов
-		json::Dict GetMap_(Handler::RequestHandler& handler, const json::Node& request);
-		void ProcessInput_();
-		void ProcessStops_();
-		void ProcessStopsDistance_();
-		void ProcessRoutes_();
-	};
-} //namespace json_reader
+        svg::Color ColorFromNode(const json::Node& node);
+    }
+    using namespace detail;
+    
+    class JsonReader {
+    public:
+        JsonReader(transport_catalogue::TransportCatalogue& catalogue, map_renderer::MapRenderer& renderer, transport_router::TransportRouter& router, serialization::Serialization& serialization);
+       
+        void ReadInput(std::istream& input);
+
+        void FillCatalogue();
+
+        void PrintRequests(std::ostream& out, transport_router::RequestHandler& request_handler);
+
+    private:
+        transport_catalogue::TransportCatalogue& catalogue_;
+        Document document_;
+        std::vector<std::unique_ptr<Request>> requests_;
+        map_renderer::MapRenderer& renderer_;
+        transport_router::TransportRouter& router_;
+        serialization::Serialization& serialization_; 
+
+        void FillDoc(std::istream& strm);
+        void FillBase(const std::vector <Node>& vec);
+        void FillStat(const std::vector<Node>& vec);
+        void FillRender(const std::map<std::string, json::Node>& dic);
+        void FillRouting(const std::map<std::string, Node>& dic);
+        void FillSerialization(const std::map<std::string, Node>& dic); 
+
+        void ProcessStopStatRequest(const transport_catalogue::TransportCatalogue::StopOutput& request, Builder& dict);
+        void ProcessBusStatRequest(const transport_catalogue::TransportCatalogue::RouteOutput& request, Builder& dict);
+    };
+}
